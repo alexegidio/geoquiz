e43d37f49fb3b6b5dc5f1678306186169c9bbe95
Alex <alex@localhost.localdomain>
2015-02-03 22:09:51 -0200

    Envio inicial do projeto

---
+++ app/src/main/java/geoquiz/com/example/alex/geoquiz/TrueFalse.java
@@ -0,0 +1,32 @@
+package geoquiz.com.example.alex.geoquiz;
+
+/**
+ * Created by alex on 08/01/15.
+ */
+public class TrueFalse {
+
+    private int mQuestion;
+
+    private boolean mTrueQuestion;
+
+    public TrueFalse(int mQuestion, boolean mTrueQuestion) {
+        this.mQuestion = mQuestion;
+        this.mTrueQuestion = mTrueQuestion;
+    }
+
+    public int getmQuestion() {
+        return mQuestion;
+    }
+
+    public void setmQuestion(int mQuestion) {
+        this.mQuestion = mQuestion;
+    }
+
+    public boolean ismTrueQuestion() {
+        return mTrueQuestion;
+    }
+
+    public void setmTrueQuestion(boolean mTrueQuestion) {
+        this.mTrueQuestion = mTrueQuestion;
+    }
+}
